6c7b33d031c5428b8e722074a4fea489074d49e6
Jay Hennessy <JAYHENNESSY@GMAIL.COM>
2015-06-20 23:08:13 -0700

    completed exact change bonfire

--- algorithms.js
+++ algorithms.js
@@ -514,9 +514,9 @@ function diff(arr1, arr2) {
       newArr[i+1] = 'invalid';
     }
   }
-  
+
   return newArr.filter(function(x){return x !== 'invalid';});
-  
+
 }
 diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
@@ -818,3 +818,98 @@ var newInv = [
 ];
 
 inventory(curInv, newInv);
+
+// --------------------------------------------------------------------------
+// Design a cash register drawer function that accepts purchase price as the
+// first argument, payment as the second argument, and cash-in-drawer (cid) as
+// the third argument.
+//
+// cid is a 2d array listing available currency.
+//
+// Return the string "Insufficient Funds" if cash-in-drawer is less than the
+// change due. Return the string "Closed" if cash-in-drawer is equal to the change due.
+//
+// Otherwise, return change in coin and bills, sorted in highest to lowest order.
+//
+function drawer(price, payment, cid) {
+  var change = payment - price;
+  var cidTotal = Math.round(cid.map(function(x){return x[1];}).reduce(function(p,c){return p+c;})*100)/100;
+  var changeArray = [];
+
+    if(cidTotal === change){return "Closed";}
+    if(cidTotal < change){return "Insufficient Funds";}
+
+    if(change >= 100){
+      var hundred = Math.floor(change/100)*100.00;
+      if(hundred>cid[8][1]){
+        hundred = cid[8][1];
+      }
+      change -= hundred;
+      changeArray.push(['ONE HUNDRED', hundred]);
+    }
+    if(change >=20){
+      var twenty = Math.floor(change/20)*20.00;
+      if(twenty>cid[7][1]){
+        twenty = cid[7][1];
+      }
+      change -= twenty;
+      changeArray.push(['TWENTY', twenty]);
+    }
+    if(change >=10){
+      var ten = Math.floor(change/10)*10.00;
+      if(ten>cid[6][1]){
+        ten = cid[6][1];
+      }
+      change -= ten;
+      changeArray.push(['TEN', ten]);
+    }
+    if(change >= 5){
+      var five = Math.floor(change/5)*5.00;
+      if(five>cid[5][1]){
+        five = cid[5][1];
+      }
+      change -= five;
+      changeArray.push(['FIVE', five]);
+    }
+    if(change >= 1){
+      var one = Math.floor(change)*1.00;
+      change -= one;
+      changeArray.push(['ONE', one]);
+    }
+    if(change >= 0.25){
+      var twentyFive = Math.floor(change/0.25)*0.25;
+      change -= twentyFive;
+      changeArray.push(['QUARTER', twentyFive]);
+    }
+    if(change >= 0.10){
+      var dime = Math.floor(change/0.10)*0.10;
+      change -= dime;
+      changeArray.push(['DIME', dime]);
+    }
+    if(change >= 0.05){
+      var nickel = Math.floor(change/0.05)*0.05;
+      change -= nickel;
+      changeArray.push(['NICKEL', nickel]);
+    }
+    if(change >= 0.01){
+      var penny = Math.floor(change/0.01)*0.01;
+      change -= penny;
+      changeArray.push(['PENNY', penny+0.01]);
+    }
+
+
+  return changeArray;
+}
+
+// Example cash-in-drawer array:
+// [['PENNY', 1.01],
+// ['NICKEL', 2.05],
+// ['DIME', 3.10],
+// ['QUARTER', 4.25],
+// ['ONE', 90.00],
+// ['FIVE', 55.00],
+// ['TEN', 20.00],
+// ['TWENTY', 60.00],
+// ['ONE HUNDRED', 100.00]]
+
+drawer(19.50, 200.00, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.10], ['QUARTER', 4.25], ['ONE', 90.00], ['FIVE', 55.00], ['TEN', 20.00], ['TWENTY', 60.00], ['ONE HUNDRED', 100.00]]);
